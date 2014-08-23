5b1acee85f136b42e088f75b57f32af9ff75a516
Seth Bicknell <seth@todaymade.com>
2014-08-23 11:23:25 -0500

    New Routes!  (Story and Employees)

--- dist/js/crossover.js
+++ dist/js/crossover.js
@@ -3,5 +3,26 @@ window.App = Ember.Application.create();
 App.Router.map(function() {
 	this.resource('app', {
 		path: '/'
+	}, function() {
+		this.resource('story', {
+			path: '/story'
+		}, function() {
+			this.resource('newStory', {
+				path: '/new'
+			}, function() {});
+			this.resource('viewStory', {
+				path: '/:id'
+			}, function() {});
+		});
+
+		this.resource('employees', {
+			path: '/employees'
+		}, function() {
+			this.resource('hire', {
+				path: '/hire'
+			});
+		});
 	});
+
+
 });
