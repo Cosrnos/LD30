718e9fcb54531964562d82f5fba029c56a4be31b
Brian Nelson <yonbeastie@gmail.com>
2014-08-23 22:32:55 -0500

    adds 'assign_techie' functionality
    Signed-off-by: Brian Nelson <yonbeastie@gmail.com>

--- src/controllers/dashboard_controller.js
+++ src/controllers/dashboard_controller.js
@@ -1,12 +1,24 @@
 App.DashboardController = Ember.ObjectController.extend({
-	needs: ['app'],
+    needs: ['app'],
 
-	actions: {
-		fire: function(employee) {
-			employee.set('hired', false);
-			if (confirm("Do you really want to fire " + employee.name + "? This can't be undone!")) {
-				this.get('controllers.app.office.employees').removeObject(employee);
-			}
-		}
-	}
+    actions: {
+        fire: function(employee) {
+            employee.set('hired', false);
+            if (confirm("Do you really want to fire " + employee.name + "? This can't be undone!")) {
+                this.get('controllers.app.office.employees').removeObject(employee);
+            }
+        },
+        assign_techie: function(employee) {
+            // Free up previous techie
+            var previous_techie = this.get('controllers.app.office.techie');
+
+            if (previous_techie) {
+                previous_techie.set('busy', false);
+            }
+
+            // Assign new techie
+            this.set('controllers.app.office.techie', employee);
+            employee.set('busy', true);
+        }
+    }
 });
