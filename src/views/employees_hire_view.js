1c42c47099f07cbfeb8a16ac26705bd76657ea1d
Alex Roth <alex@coschedule.com>
2014-08-23 21:35:56 -0500

    [CHORE] Styles the hire employee route
    Signed-off-by: Alex Roth <alex@coschedule.com>

---
+++ src/views/employees_hire_view.js
@@ -0,0 +1,42 @@
+App.HireView = Em.View.extend({
+	currentIndex: 0,
+	startOfList: function() {
+		return this.get('currentIndex') === 0;
+	}.property('currentIndex'),
+	endOfList: function() {
+		return this.get('currentIndex') === this.get('controller.model.length') - 1;
+	}.property('currentIndex', 'controller.model.length'),
+	actions: {
+		prev: function() {
+			if (this.get('startOfList') === false) {
+				var cindex = this.get('currentIndex');
+
+				cindex--;
+
+				var elems = this.$().find('.hire-employee.employee');
+				var scrollTo = $(elems[cindex]);
+				var container = this.$().find('.hire-employee.container');
+
+				container.animate({
+					scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+				}, 500);
+				this.set('currentIndex', cindex);
+			}
+		},
+		next: function() {
+			if (this.get('endOfList') === false) {
+				var cindex = this.get('currentIndex');
+				cindex++;
+
+				var elems = this.$().find('.hire-employee.employee');
+				var scrollTo = $(elems[cindex]);
+				var container = this.$().find('.hire-employee.container');
+
+				container.animate({
+					scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+				}, 500);
+				this.set('currentIndex', cindex);
+			}
+		}
+	}
+});
