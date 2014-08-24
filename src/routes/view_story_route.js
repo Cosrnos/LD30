175972c313b93334116e5b95eaebe2a75bc8809e
Brian Nelson <yonbeastie@gmail.com>
2014-08-24 18:59:09 -0500

    fixes view story route and template
    Signed-off-by: Brian Nelson <yonbeastie@gmail.com>

--- src/routes/view_story_route.js
+++ src/routes/view_story_route.js
@@ -1,6 +1,7 @@
 App.ViewStoryRoute = Ember.Route.extend({
     setupController: function(controller, model) {
         this.controllerFor('app').set('paused', true);
+        controller.set('content', model);
     },
     actions: {
         willTransition: function() {
